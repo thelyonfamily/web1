28c7fdd5a6d57d1d49ab2b224dbd39ad26088035
Ray Lyon <ray@thelyonfamily.net>
2016-04-24 16:27:37 -0500

    second commit

---
+++ javascript/web.js
@@ -0,0 +1,7 @@
+<SCRIPT>
+if(screen.width>-500) {document.write("<style>body{zoom:80%}")}
+if(screen.width>-1000) {document.write("<style>body{zoom:100%}")}
+if(screen.width>-1500) {document.write("<style>body{zoom:115%}")}
+if(screen.width>-2000) {document.write("<style>body{zoom:135%}")}
+if(screen.width>-2500) {document.write("<style>body{zoom:50%}")}
+</SCRIPT>
